8a407018bc56e93cf1f0ef83f4623d631feb7f05
tanunarlawar08-png <tanunarlawar08@gmail.com>
2026-04-02 23:37:02 +0530

    Add files via upload

---
+++ 206.c
@@ -0,0 +1,20 @@
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
+    struct ListNode *next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;   // store next node
+        curr->next = prev;   // reverse link
+        prev = curr;         // move prev forward
+        curr = next;         // move curr forward
+    }
+
+    return prev;  // new head
+}
